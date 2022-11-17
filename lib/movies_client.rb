fe2ceb256a066cfdfb559415aac57628c8442270
Tamas Kopasz <ktomi93@gmail.com>
2022-11-17 23:33:21 +0100

    Init requests with search and get_genres

--- lib/movies_client.rb
+++ lib/movies_client.rb
@@ -1,7 +1,24 @@
 ##
 # This class responsible to process movies list
 class MoviesClient
-  def search
+
+  def search query
+    RestUtil.make_request "search/movie", {:api_key => Rails.application.credentials.api_key, :query => query}
+  end
+
+  def discover
+  end
+
+  def configuration
+  end
+
+  def get_image
+  end
+
+  def get_movie
+  end
+
+  def get_genres language
+    RestUtil.make_request "genre/movie/list", {:api_key => Rails.application.credentials.api_key, :language => language}
   end
 end
-
