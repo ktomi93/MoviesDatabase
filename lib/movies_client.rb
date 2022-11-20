89206e5669b15c72cf524b69547ffac0985148b7
Tamas Kopasz <ktomi93@gmail.com>
2022-11-20 01:48:24 +0100

    MoviesClient update

--- lib/movies_client.rb
+++ lib/movies_client.rb
@@ -2,24 +2,20 @@
 # This class responsible to process movies list
 class MoviesClient
 
-  def search query
-    RestUtil.make_request "search/movie", {:api_key => API_KEY, :query => query}
-  end
-
-  def discover
+  def search(query, page = 1)
+    RestUtil.make_request "search/movie", { :api_key => API_KEY, :query => query, :page => page }
   end
 
   def get_configuration
     RestUtil.make_request "configuration", {:api_key => API_KEY}
   end
 
-  def get_image
+  def get_genres(language)
+    RestUtil.make_request "genre/movie/list", { :api_key => API_KEY, :language => language }
   end
 
-  def get_movie
+  def discover(page = 1)
+    RestUtil.make_request "/discover/movie", { :api_key => API_KEY, :page => page }
   end
 
-  def get_genres language
-    RestUtil.make_request "genre/movie/list", {:api_key => API_KEY, :language => language}
-  end
 end
