9e8c7347ea615f280a9d43432fc1e8eda2bc4979
Tamas Kopasz <ktomi93@gmail.com>
2022-11-25 20:08:53 +0100

    partial refactor, cached notification, movie info and tooltips

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,34 @@
 class ApplicationController < ActionController::Base
 
-  # after_action :get_genres, :only => [:home]
-  # after_action :get_configuration, :only => [:home]
   before_action :init
 
+  @@language_map = {:en => "en-US"}
+
   def home
-    result = @client.discover @current_page
-    @movies = result["results"]
-    # logger.info RestUtil.make_request "movie/550", {:api_key => "9b27b7cd4dcd6fa71a7df5ee7f66038c1"}
+    result = @client.discover @current_page_number
+    @cached = result[:cached]
+    result = result[:result]
+    if result
+      @movies = result["results"]
+    end
   end
 
   def search
-    result = @client.search params[:search_query], @current_page
-    @movies = result["results"]
-    @total_pages = result["total_pages"]
-    @total_results = result["total_results"]
+    @searched_query = params[:search_query]
+    result = @client.search @searched_query, @current_page_number
+    @cached = result[:cached]
+    result = result[:result]
+    if result
+      @movies = result["results"]
+      @total_pages = result["total_pages"]
+      @total_results = result["total_results"]
+    end
   end
-
-  # def get_genres
-  #   @genres = @client.get_genres "en-US"
-  # end
-  #
-  # def get_configuration
-  #   @genres = @client.get_configuration
-  # end
 
   def init
     @client = MoviesClient.new
-    @current_page = params[:page].present? ? params[:page].to_i : 1
+    @current_page_number = params[:page].present? ? params[:page].to_i : 1
+    @genres = @client.get_genres(@@language_map[I18n.locale])
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,28 @@
 module ApplicationHelper
-  def get_poster_image image_path
+  def get_poster_image(image_path, title)
     if image_path.present?
-      image_tag "#{BASE_IMAGE_PATH}t/p/w154#{image_path}"
+      image_tag "#{BASE_IMAGE_PATH}t/p/w154#{image_path}", class: "image-size", alt: title
     else
-      image_tag "default-poster.jpg"
+      image_tag "default-poster.jpg", alt: title
     end
   end
 
   def get_start_interval
-    @current_page < 5 ? 1 : @current_page - 3
+    @current_page_number < 5 ? 1 : @current_page_number - 3
   end
 
   def get_end_interval
-    @total_pages.blank? || @current_page + 3 < @total_pages ? @current_page + 3 : @total_pages
+    @total_pages.blank? || @current_page_number + 3 < @total_pages ? @current_page_number + 3 : @total_pages
   end
 
   def page_navigation_path(path, page_number)
     method(path).call(:search_query => params[:search_query], :page => page_number)
+  end
+
+  def genres ids
+    ids.map do |id|
+      @genres[id]
+    end.join(", ")
   end
 
 end
